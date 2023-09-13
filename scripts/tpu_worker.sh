8c0e40bd620cbe1fe8934a75af976fe7b333d0af
Francis Lewis <glewis17@cs.stanford.edu>
2023-09-12 20:55:11 -0400

    Mount GCP bucket to TPU VM (#5)

--- scripts/tpu_worker.sh
+++ scripts/tpu_worker.sh
@@ -8,5 +8,15 @@ gke_oslogin_username=$(gcloud compute os-login describe-profile | awk '/username
 echo "gke service account: $gke_service_account"
 echo "gke_oslogin_username: $gke_oslogin_username"
 
+export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
+echo "deb https://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install gcsfuse
+
+sudo mkdir -p /mnt/disks/bucket
+sudo chmod -R 777 /mnt/disks/bucket
+gcsfuse -o allow_other --file-mode=777 --dir-mode=777 <your-bucket-here> /mnt/disks/bucket
+
 sudo gcloud auth configure-docker us-central1-docker.pkg.dev
 sudo docker pull <path-to-docker-repo-here>
